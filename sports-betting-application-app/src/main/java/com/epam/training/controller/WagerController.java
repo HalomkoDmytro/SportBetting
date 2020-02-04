40789c76a7a46557ec50f082c8e2ef0255b3bc7f
HalomkoDmytro <galomko.dmytro@gmail.com>
2020-02-04 14:42:32 +0200

    update wager page

--- sports-betting-application-app/src/main/java/com/epam/training/controller/WagerController.java
+++ sports-betting-application-app/src/main/java/com/epam/training/controller/WagerController.java
@@ -1,10 +1,10 @@
 package com.epam.training.controller;
 
-import com.epam.training.dao.SportEventDao;
 import com.epam.training.dto.impl.WagerDto;
 import com.epam.training.model.bet.Bet;
 import com.epam.training.model.sportevent.SportEvent;
 import com.epam.training.service.BetService;
+import com.epam.training.service.SportEventsService;
 import com.epam.training.service.WagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,28 +15,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class WagerController {
 
     private final WagerService wagerService;
     private final BetService betService;
-    private final SportEventDao sportEventDao;
+    private final SportEventsService sportEventsService;
 
     @Autowired
-    public WagerController(WagerService wagerService, BetService betService, SportEventDao sportEventDao) {
+    public WagerController(WagerService wagerService, BetService betService, SportEventsService sportEventsService) {
         this.wagerService = wagerService;
         this.betService = betService;
-        this.sportEventDao = sportEventDao;
+        this.sportEventsService = sportEventsService;
     }
 
-
-    @GetMapping("/wagerNew/{id}")
+    @GetMapping("/wagerNew/{idSportEvent}")
     public String getBet(@PathVariable int idSportEvent, Model model) {
-        Optional<SportEvent> sportEvent = sportEventDao.findById(idSportEvent);
+        final SportEvent sportEvent = sportEventsService.byId(idSportEvent);
         List<Bet> bets = betService.betsForSportEventById(idSportEvent);
-        model.addAttribute("sportEvent", sportEventDao);
+        model.addAttribute("sportEvent", sportEvent);
         model.addAttribute("bets", bets);
         return "wagerNew";
     }
--- sports-betting-application-app/src/main/resources/static/js/sport-betting.js
+++ sports-betting-application-app/src/main/resources/static/js/sport-betting.js
@@ -1,5 +1,9 @@
 var sportBetting = {
     goToHrefOnClickRow: function (thisObj) {
         window.document.location = thisObj.children('td:first').data("href");
+    },
+
+    onBetTypeChange: function (thisObj) {
+        let selectedOption = $('#selectedOption').children("option:selected").val();
     }
 }
