3eabe5d0c3e379a4acec7d18119bf64056ba4be1
HalomkoDmytro <galomko.dmytro@gmail.com>
2020-02-04 17:50:56 +0200

    ajax request for get outcome

--- sports-betting-application-app/src/main/java/com/epam/training/controller/WagerController.java
+++ sports-betting-application-app/src/main/java/com/epam/training/controller/WagerController.java
@@ -3,8 +3,10 @@ package com.epam.training.controller;
 import com.epam.training.dto.impl.WagerDto;
 import com.epam.training.model.bet.Bet;
 import com.epam.training.model.sportevent.SportEvent;
+import com.epam.training.model.user.Player;
 import com.epam.training.service.BetService;
 import com.epam.training.service.SportEventsService;
+import com.epam.training.service.UserService;
 import com.epam.training.service.WagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,20 +24,28 @@ public class WagerController {
     private final WagerService wagerService;
     private final BetService betService;
     private final SportEventsService sportEventsService;
+    private final UserService userService;
 
     @Autowired
-    public WagerController(WagerService wagerService, BetService betService, SportEventsService sportEventsService) {
+    public WagerController(WagerService wagerService, BetService betService,
+                           SportEventsService sportEventsService, UserService userService) {
         this.wagerService = wagerService;
         this.betService = betService;
         this.sportEventsService = sportEventsService;
+        this.userService = userService;
     }
 
     @GetMapping("/wagerNew/{idSportEvent}")
     public String getBet(@PathVariable int idSportEvent, Model model) {
         final SportEvent sportEvent = sportEventsService.byId(idSportEvent);
         List<Bet> bets = betService.betsForSportEventById(idSportEvent);
+
+        //todo remove user userService
+        final Player player = userService.findPlayerById(1);
+
         model.addAttribute("sportEvent", sportEvent);
         model.addAttribute("bets", bets);
+        model.addAttribute("player", player);
         return "wagerNew";
     }
 
--- sports-betting-application-app/src/main/resources/static/js/sport-betting.js
+++ sports-betting-application-app/src/main/resources/static/js/sport-betting.js
@@ -5,7 +5,7 @@ var sportBetting = {
 
     onBetTypeChange: function (thisObj) {
         let selectedOption = $('#selectedOption').children("option:selected");
-        let text = selectedOption.val();
+        // let text = selectedOption.val();
         let betIdSelectedOption = selectedOption.data("idoption")
         sportBetting.getContent("/outcome/" + betIdSelectedOption);
     },
@@ -13,15 +13,33 @@ var sportBetting = {
     getContent: function (url) {
         $.ajax({
             url: url,
+            type: "GET",
+            contentType: "application/json",
             dataType: "json",
-            success: function (data) {
-                if (data != null) {
-                    window.alert(data);
-                }
+            // data: JSON.stringify(),
+            cache: false,
+            timeout: 600000,
+            success: function (json) {
+                sportBetting.populateDropDown(json);
             },
             error: function (data) {
                 window.alert("something go wrong, try again latter...")
             }
         })
+    },
+
+    populateDropDown: function (json) {
+        let outcomeOptionBox = $('#selectOutcomeOption');
+        let outcomeOptionBlock = $('#outcomeOptionBlock');
+        outcomeOptionBox.empty();
+
+        if (json.length !== 0) {
+            outcomeOptionBlock.css('visibility', 'visible');
+            $.each(json, function (i, value) {
+                outcomeOptionBox.append($('<option></option>').attr('value', value.value).text(value.value));
+            })
+        } else {
+            outcomeOptionBlock.css('visibility', 'hidden');
+        }
     }
 }
